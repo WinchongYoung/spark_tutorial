e3c0d1a7183b7a243840c0781879d40592f28b27
WinchongYoung <ywz745827328@gmail.com>
2023-08-06 17:20:34 +0800

    clear cached

--- src/main/java/com/atguigu/bigdata/spark/rdd/persist/Spark01_RDD_Persist.scala
+++ src/main/java/com/atguigu/bigdata/spark/rdd/persist/Spark01_RDD_Persist.scala
@@ -24,17 +24,19 @@ object Spark01_RDD_Persist {
         )
         // 设定数据持久化
         // cache方法可以将血缘关系进行修改，添加一个和缓存相关的依赖关系
-        // cache操作不安全。
-        wordToOne.cache()
+        // cache默认持久化级别为 MEMORY_ONLY 所以cache操作不安全。
+        // 注意  使用了wordToOne.cache() 就不能使用persist方法
+        // 否则会抛出异常 Cannot change storage level of an RDD after it was already assigned a level
+        // wordToOne.cache()
         // 如果持久化的话，那么持久化的文件只能自己用。而且使用完毕后， 会删除
-        wordToOne.persist(StorageLevel.DISK_ONLY_2)
+        wordToOne.persist(StorageLevel.MEMORY_AND_DISK)
 
         val wordToCount = wordToOne.reduceByKey(_+_)
         println(wordToCount.toDebugString)
         wordToCount.collect()//.foreach(println)
         println("--------------------------------------------")
-       // val rdd2: RDD[(Int, Iterable[(String, Int)])] = wordToOne.groupBy(_._2)
-       // rdd2.collect()
+        val rdd2: RDD[(Int, Iterable[(String, Int)])] = wordToOne.groupBy(_._2)
+        rdd2.collect()
         println(wordToCount.toDebugString)
 
         sc.stop()
